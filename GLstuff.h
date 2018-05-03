7ac554b91ed086b862f386a0f01a64deb5463d0c
inakietxeba <i.echevarriaherrera@gmail.com>
2018-05-03 15:14:08 +0200

    hg

--- GLstuff.h
+++ GLstuff.h
@@ -2,13 +2,18 @@
  
 
 
-
-/*#ifdef __WIN32
-#include "glut.h"  //PARA QUE FUNCIONE EN WINDOWS ES NECESARIO ESTE
-#endif*/
-
-//#ifdef __APPLE__
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
 
 
 
