1f1eec16b377d820e7cfa35aed4e6df604fdcbf7
inakietxeba <i.echevarriaherrera@gmail.com>
2018-05-03 16:51:47 +0200

    g

---
+++ Figura.cpp
@@ -0,0 +1,48 @@
+
+#include "Figura.h"
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+
+//INICIALIZAR
+
+Figura::Figura()
+{
+    pos = {0,0,0};
+}
+
+
+
+void Figura::getNombre(char *n)
+{
+  strcpy(n,nombre);
+}
+
+
+void Figura::setNombre(char *n)
+{
+  strcpy(nombre,n);
+}
+
+
+void Figura::mover(float x,float y,float z)
+{
+  
+
+
+    }
+void Figura::rotar(float a, float[])
+{
+    
+    
+    
+}
+
+    
+}
+  
+
---
+++ Figura.h
@@ -0,0 +1,30 @@
+
+#ifndef Figura
+#define Figura
+
+#include "personaje.h"
+
+
+class Figura{
+ private:
+pos= float[3];
+rot= float[2];
+
+
+ public:
+  Figura();
+  
+    
+    void getNombre(char *n);
+    void setNombre(char *n);
+ // void addFigura(Figura*);
+    void mover(float, float ,float);
+    void rotar (float,float[3]);
+    virtual void draw()=0;
+};
+
+#endif
+
+
+
+
