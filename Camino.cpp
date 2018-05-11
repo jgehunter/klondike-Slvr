40376650432ed8b2bcdc0fce3f66f9a1ff139c98
inakietxeba <i.echevarriaherrera@gmail.com>
2018-05-11 12:33:45 +0200

    w

--- Camino.cpp
+++ Camino.cpp
@@ -18,32 +18,32 @@ void Camino::draw()
 
     glPushMatrix();
     
-    glTranslatef(-0.25f,0,0);
+    glTranslatef(-0.1f,0,0);
    // glScalef(0.2, 0.2, 0.2);
     glColor3f( 255/255.0, 0/255.0, 0/255.0);
    
-    
-    glBegin(GL_POLYGON);
-    glColor3f( 255/255.0, 0/255.0, 0/255.0);
-    glVertex3f(0,0,0.1);
-    glVertex3f(0.3,0,0.1);
-    glVertex3f(0.5,2,0.1);
-    glVertex3f(0.3,4.2,0.1);
-    glVertex3f(0.6,2,0.1);
-    glVertex3f(0,4.2,0.1);
    
-    
+      glScalef(0.5, 0.5, 1);
+    glBegin(GL_POLYGON);
+  
+    glColor3f( 255/255.0, 0/255.0, 0/255.0);
+    glVertex3f(0.2,0,0.1);
+    glVertex3f(0.5,2,0.1);
+    glVertex3f(0.2,4.2,0.1);
+    glVertex3f(-0.2,4.2,0.1);
+    glVertex3f(0.2,3,0.1);
+    glVertex3f(-0.2,0,0.1);
+   
     glEnd();
     
     
-    glTranslatef(0.0f,4,0);
     
+    glTranslatef(0.0f,4,0);
     glBegin(GL_TRIANGLES);
     
-    glVertex3f(0.3f, 0, 0.1f);
+    glVertex3f(0.5f, 0, 0.1f);
     glVertex3f( 0.0f,  0.5f, 0.1f);
-    glVertex3f(-0.3f, 0, 0.1f);
-    
+    glVertex3f(-0.5f, 0, 0.1f);
     
     glEnd();
 
