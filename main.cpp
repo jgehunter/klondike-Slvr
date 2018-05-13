32f53ca58709790068631c3c044343b3ccbf7ea0
jgehunter <jgehunter@gmail.com>
2018-05-13 16:39:02 +0200

    Retoques

--- main.cpp
+++ main.cpp
@@ -26,13 +26,25 @@
 
 #ifdef _WIN32
 #include <windows.h>
-#include <chrono>
-#include <thread>
 #endif
 
 
 
 // PROTOTIPOS
+
+// Función para ralentizar el programa
+void mySleep(int sleepMs)
+{
+#ifdef __linux__
+    usleep(sleepMs * 1000);   // usleep takes sleep time in us (1 millionth of a second)
+#endif
+#ifdef __APPLE__
+    usleep(sleepMs * 1000);   // usleep takes sleep time in us (1 millionth of a second)
+#endif // __APPLE__
+#ifdef _WIN32
+    Sleep(sleepMs);
+#endif
+}
 
 //Callbacks de logica del programa
 void myLogic();
@@ -106,73 +118,72 @@ GLuint Bgtexture;
 //Main contiene las inicializaciones + bucle infinito
 int main(int argc,char* argv[])
 {
-    
+
     // Inicializaciones programa logica
-    
+
     ficheroTablero.open("Tablero.txt");
-    
+
     while(ficheroTablero >> posX >> posY >> mov){
         tablero[std::make_pair(posX,posY)] = mov;
     }
-    
+
     nodoActual = new Nodo;
     nodoPlaceholder = new Nodo;
     nodoInicio = new Nodo;
-    
+
     nodoActual->Setcoordenadas(std::make_pair(0,0));
     nodoActual->Setparent(nodoPlaceholder);
     nodoPlaceholder->Setparent(nullptr);
     nodoInicio = nodoActual;
-    
+
     OPEN.push_back(nodoActual);
-    //CLOSED.push_back(*nodoActual);
-    
+
     contador = 0;
-    
+
     flagPath = 0;
     flagExit = 0;
     flagEnd = 0;
-    
-    
+
+
     // Inicializaciones OPEN GL
-    
+
     // Inicialización GLUT
     glutInit(&argc, argv);
-    
+
     // Inicializaciones openGL (ver GLstuff.c)
     init_GL_stuff();
-    
+
     // Definición call backs de GLUT
     glutDisplayFunc(OnDibuja);
-    
+
     glutIdleFunc(myLogic);
-    
+
     glutKeyboardFunc(OnKeyboardDown);
     glutSpecialFunc(OnSpecKeyboardDown);
-    
-    
+
+
     // posicciona el punto de vista
     gluLookAt(cam_pos[0],cam_pos[1],cam_pos[2],       // posicion del  ojo
               0.0, 0, 0.0,    // hacia que punto mira
               0.0, 1.0, 0.0);     // vector "UP"  (vertical positivo)
-    
+
     // Carga la textura
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    
-    
+
+
     KLtexture= LoadTexture(900,1074,  "klondike.bmp");  //NO FUNIONA SI NO PONES el .bmp donde se crea el .exe
     glBindTexture (GL_TEXTURE_2D, KLtexture);
-    
-    
+
+
     Bgtexture=LoadTexture(800,500, "montana.bmp" );
-    
-    
+
+
     glBindTexture (GL_TEXTURE_2D, Bgtexture);
-    
-    
+
+
     // bucle del programa
     glutMainLoop();
-    
+
     return 0;
 }
 
@@ -186,86 +197,50 @@ void myLogic()  // CONTIENE LAS ACTUALIZACIONES DEL PROGRAMA
 {
     /**************************************************************/
 
-#ifdef __linux__
-	usleep(50000);
-#endif
-    
-#ifdef __APPLE__
-    usleep(50000);
-#endif
-    
-#ifdef _WIN32
-	std::this_thread::sleep_for(std::chrono::microseconds(50000));
-#endif
-	
-	
-    
-    
+    //mySleep(50);
+
     // PROGRAMA DE HUNTER QUE DA LAS Xp Yp Zp , Y LAS FUNCIONES QUE TRASLADAN EL MU„ECO (esto es un bucle ya de por si)
-    
+
     if(!flagExit){
         nodoActual = OPEN.front();
         OPEN.pop_front();
         nodoActual->Crearhijos( CLOSED, OPEN, nodoActual, tablero );
-        
+
         for( auto iter : nodoActual->Gethijos()){
             OPEN.push_back(iter);
             contador++;
             CLOSED.push_back(*iter);
         }
-        
+
         if( OPEN.empty() || nodoActual->Comprobarmovimiento( tablero ) == 0 ) flagExit = 1;
-        
+
         std::cout << "x y actuales: " << nodoActual->Getcoordenadas().first << " " << nodoActual->Getcoordenadas().second << std::endl;
-        
+
         xp=0.42*nodoActual->Getcoordenadas().first;
         yp=-0.15+0.35*nodoActual->Getcoordenadas().second;
-        
-        
-        
-        nodoVisitado1 = nodoActual;
-        
+
     } else {
-        nodoInicio = nodoActual;
-        
         if(!flagPath){
-            while(nodoVisitado1->Getparent() != nullptr) {
-                recorrido.push_back(*nodoVisitado1);
-                nodoVisitado1 = nodoVisitado1->Getparent();
+            while(nodoActual->Getparent() != nullptr) {
+                recorrido.push_back(*nodoActual);
+                nodoActual = nodoActual->Getparent();
             }
             flagPath = 1;
         }
-        /* if(!flagClear){
-         int flagLeaf = 0;
-         do {
-         if(nodoInicio->Gethijos().size() == 0) flagLeaf = 1;
-         if(!flagLeaf) nodoInicio = nodoInicio->Gethijos().back();
-         if(flagLeaf){
-         Nodo* nodoTemp;
-         nodoTemp = nodoInicio->Getparent();
-         delete nodoInicio;
-         nodoInicio = nodoTemp;
-         nodoInicio->Gethijos().pop_back();
-         if(nodoInicio->Getparent() == nullptr) flagClear = 1;
-         }
-         flagLeaf = 0;
-         } while(1) ;
-         } */
     }
-    
-    if(!flagEnd){
+
+    if(flagExit && flagPath && !flagEnd){
         int paso;
         paso = 0;
         for(auto iter: recorrido){
             std::cout << "El paso " << paso << " es " << iter.Getcoordenadas().first << " " << iter.Getcoordenadas().second << std::endl;
+            paso++;
         }
         flagEnd = 1;
     }
-    
-    //std::cout << "La posicion final es: " << nodoActual->Getcoordenadas().first << " " << nodoActual->Getcoordenadas().second << std::endl;
-    
+
     /**************************************************************/
-    
+
 
 }
 
@@ -276,50 +251,55 @@ void OnDibuja(void)
     //Borrado de la pantalla
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     /**************************************************************/
-    
+
     // proyeccion de las figuras     DIBUJAR
-    
+
     displaytext(KLtexture);
     background(Bgtexture);
-    
-    
+
+
     pollito.mover(xp, yp, zp);
     pollito.draw();
-   
+
    camino.draw();
-   
-    
- 
-     
+
+
+
+
      if(flagEnd){
-     
-         for(auto iterador : recorrido){
-          camino.siguientes(iterador.Getcoordenadas().first, iterador.Getcoordenadas().second);
-          camino.draw();
-         }
+        std::vector<Nodo>::reverse_iterator rit = recorrido.rbegin();
+        int BORRAR = 0;
+        do{
+            std::cout << "DATO "  << BORRAR << std::endl;
+            std::cout << "x e y: " << rit->Getcoordenadas().first << " " << rit->Getcoordenadas().second << std::endl;
+            camino.siguientes(rit->Getcoordenadas().first, rit->Getcoordenadas().second);
+            camino.draw();
+            BORRAR++;
+            rit++;
+        } while(rit != recorrido.rend());
         }
-     
-     
- 
-    
-    
+
+
+
+
+
     pollito.personaje_rot += 2.0;
     if(pollito.personaje_rot>360) pollito.personaje_rot = 0;
-    
-    
-    
-    
+
+
+
+
     glLoadIdentity();
     /**************************************************************/
     // posicciona el punto de vista
     gluLookAt(cam_pos[0],cam_pos[1],cam_pos[2],  // posicion del  ojo
               0.0, 0, 0.0,                // hacia que punto mira
               0.0, 1.0, 0.0);         // vector "UP"  (vertical positivo)
-    
+
     //Al final, cambiar el buffer
     glutSwapBuffers();
     glutPostRedisplay();//se le indica que redibuje la pantalla
-    
+
 }
 
 
